92d80f13a2b8ee32ea042a94232ff9a1f0c37728
Theodore Thomas <55268314+t3ddyt369@users.noreply.github.com>
2024-06-27 11:38:58 +0200

    Updates to structure, created main.js file

---
+++ frontend/main.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+	getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+	let count = 30;
+	fetch(functionApi).then(response => {
+		return response.json()
+	}).then(response =>{
+			console.log("Website called function API.");
+			count = reponse.count;
+			document.getElementById("counter").innerText = count;
+		}).catch(function(err){
+		console.log(error);
+	});
+	return count;
+}
+		
