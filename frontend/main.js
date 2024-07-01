8d2e564bf2d9198545878c6e81dcc19cd5a4710e
Theodore Thomas <55268314+t3ddyt369@users.noreply.github.com>
2024-07-01 10:55:08 +0200

    sync repo and local files

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 	getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://git-resume-counter.azurewebsites.net/api/GetResumeCounter?code=a-vtEfCNU7_oRO2f-GgGR6igyxTFHb6K9wFt4GAygvgVAzFuOk4GPA%3D%3D'
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
 	let count = 30;
-	fetch(functionApi).then(response => {
+	fetch(functionApiUrl).then(response => {
 		return response.json()
 	}).then(response =>{
 		console.log("Website called function API.");
